f930abe8174ac10c883edbe4686fc29d3dc97410
Igor Oliveira <igoroliveira9827@gmail.com>
2024-03-13 22:34:29 -0300

    feat: create a new publisher

---
+++ OrderService/src/Infrastructure/publisher.ts
@@ -0,0 +1,20 @@
+import amqp from 'amqplib';
+import { OrderDTO } from '../Domain/Entities/OrderDto';
+
+export class Connect 
+{
+    async connectRMQ(data: OrderDTO)
+    {
+        const connection = await amqp.connect('amqp://localhost:5672');
+        const channel = await connection.createChannel();
+
+        const result = await channel.assertQueue("orders");
+
+        channel.sendToQueue("jobs", Buffer.from(JSON.stringify(data)));
+
+        console.log(`Job sent successfully!!\n ${{data}}`);
+
+        channel.close();
+    }
+
+}
