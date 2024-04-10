7ccecf503cd8e12d00814e686aaf226a2b02c856
Igor Oliveira <igoroliveira9827@gmail.com>
2024-04-10 20:54:36 -0300

    feat: add a new configuration for route in Kitchen Service

--- OrderKitchenService/src/server.ts
+++ OrderKitchenService/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { Connect } from './Infrastructure/consumer';
+import route from './Presentation/Routes/route';
 
 const app = express();
 
@@ -7,6 +8,7 @@ const connectQueue = new Connect();
 
 app.use(express.json());
 app.use(connectQueue.connectRMQ);
+app.use(route);
 
 
 app.listen(3001, () => console.log("Kitchen service is running at port 3001!"));
