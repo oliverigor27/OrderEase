1fb9ad8ccbd6717456c3b1364c3f6c8dd80026ab
Igor Oliveira <igoroliveira9827@gmail.com>
2024-04-10 20:56:54 -0300

    feat: add a new method in kitchen service

--- OrderKitchenService/src/Application/Services/kitchenService.ts
+++ OrderKitchenService/src/Application/Services/kitchenService.ts
@@ -1,9 +1,22 @@
-import { Connect } from "../../Infrastructure/consumer";
-
 export class KitchenService
 {
-    async KitchenConsumer()
-    {
+    private _arr: Object[];
 
+    constructor() {
+        this._arr = [];
+    }
+
+    async KitchenConsumer(data: Object)
+    {
+        
+        this._arr.push(data);
+
+        console.log(this._arr);
+
+    }
+
+    async GetOrders()
+    {
+        return this._arr;
     }
 }
